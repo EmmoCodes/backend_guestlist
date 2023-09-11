602c641ef1fffcda4490c41cf18479cbf0db778a
S4nt1nO <Gianluca1996@gmx.de>
2023-09-11 14:58:01 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import fs from 'fs/promises'
+import { existsSync } from 'fs'
+import multer from 'multer'
+import { body, validationResult } from 'express-validator'
 
 const PORT = 9898
 const app = express()
@@ -8,18 +11,39 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const guestComments = []
+const upload = multer()
+
+let guestComments = []
 const historyPath = new URL('./public/history.json', import.meta.url)
 
-app.post('/api/comments', (req, res) => {
-  const comment = req.body
-  console.log(comment)
-  guestComments.push(comment)
-  fs.writeFile(historyPath, JSON.stringify(guestComments) + '\n', { encoding: 'utf-8' })
-  res.end()
-})
+app.post(
+  '/api/comments',
+  upload.none(),
+  body('firstname').notEmpty(),
+  body('lastname').notEmpty(),
+  body('email').notEmpty(),
+  body('message').notEmpty(),
+  (req, res) => {
+    const result = validationResult(req)
+    if (result.errors.length > 0) {
+      res.status(401).end()
+      return
+    }
+    const comment = req.body
 
-app.get('/api/comments', (_, res) => {
+    guestComments.push(comment)
+    fs.writeFile(historyPath, JSON.stringify(guestComments, null, 2), { encoding: 'utf-8' })
+    res.end()
+  },
+)
+
+app.get('/api/comments', async (_, res) => {
+  if (existsSync(historyPath)) {
+    await fs
+      .readFile(historyPath, { encoding: 'utf-8' })
+      .then(response => JSON.parse(response))
+      .then(data => (guestComments = data))
+  }
   res.json(guestComments)
 })
 
---
+++ validationTrucks.js
@@ -0,0 +1,20 @@
+export const formSchema = {
+  firstname: {
+    errorMessage: 'Must not be empty',
+    notEmpty: true,
+  },
+  lastname: {
+    errorMessage: 'Must not be empty',
+    notEmpty: true,
+  },
+  email: {
+    errorMessage: 'We need an email',
+    notEmpty: true,
+  },
+  message: {
+    errorMessage: 'We need an email',
+    notEmpty: true,
+  },
+}
+
+export const transporteSchema = {}
